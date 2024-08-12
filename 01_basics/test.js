756fa5114e9dbea51106be702c4f4c4536938bb1
Jitendra Mandal <100280845+Jittumandal@users.noreply.github.com>
2024-08-12 10:16:32 +0000

    container configer

---
+++ 01_basics/test.js
@@ -0,0 +1,12 @@
+console.log("jitendra mandal")
+
+//  var a = 10;
+//  var b = 10 ;
+//  var c = a + b ;
+
+// console.log(c);
+
+// generating a random number
+const a = Math.random() * (10-1) + 1
+console.log(`Random value between 1 and 10 is ${a}`);
+
