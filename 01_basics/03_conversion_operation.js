aa2a7e6fdecdd8fe4e28d8a1bed6d67743f8ce25
Jitendra Mandal <100280845+Jittumandal@users.noreply.github.com>
2024-08-20 10:57:55 +0000

    conversion operation

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,24 @@
+let score = undefined
+// console.log(typeof score)
+// console.log(typeof (score))
+
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// note 
+// string "33" = print is 33
+// number 33 = print is 33
+// NaN or undefined and  "33abc" = print is NaN (not a number)
+// null = print is 0 zero 
+
+let loggedIn ="jitendra"
+let booleanloggedIn = String(loggedIn)
+console.log(booleanloggedIn)
+
+// put 1 = print is true
+// put true = print is true
+// put flase = print flase 
+// put string "name" = print name  is true
