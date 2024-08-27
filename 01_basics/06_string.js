4a2577c33d62d858707b1a85ed071560197be05a
Jitendra Mandal <100280845+Jittumandal@users.noreply.github.com>
2024-08-27 12:29:49 +0000

    STRING

---
+++ 01_basics/06_string.js
@@ -0,0 +1,18 @@
+const name = "Mandal"
+const repCount = 80
+
+// console.log(name +   repCount +   "value")  // this is old javascript concept 
+// console.log(`my name is ${name} and repocount value is ${repCount}`) // this is advance ECMA Script concept this is best programing 
+
+
+// ye wala ek new tarika hai strign difined karne ka 
+const gameName = new String ('Mandal')
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length)
+// console.log(gameName.toLowerCase())
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(3));
+console.log(gameName.indexOf('M'))
