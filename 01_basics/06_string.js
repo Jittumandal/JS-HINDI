851b82013e3a8b201aefe8dea98148812263c374
Jitendra Mandal <100280845+Jittumandal@users.noreply.github.com>
2024-09-02 05:39:22 +0000

    string

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -16,3 +16,21 @@ const gameName = new String ('Mandal')
 // console.log(gameName.toUpperCase())
 // console.log(gameName.charAt(3));
 console.log(gameName.indexOf('M'))
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
